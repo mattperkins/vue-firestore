a16ac7db3daedacded350f35d5b9f5d66752f02b
mattperkins <github@mattperkins.com>
2017-12-16 01:30:53 +0000

    add viewuser and delete functionality

--- src/components/ViewUser.vue
+++ src/components/ViewUser.vue
@@ -1,16 +1,74 @@
 <template>
    <div id="viewUser">
-    <h3>{{masthead}}</h3>
+   <h3>{{masthead}}</h3>
+      <ul>
+        <li style="font-size: 1.6rem; color:#333;">
+          {{name}}
+        </li>
+        <li style="font-size: 1.6rem; color:#333;">
+          {{position}}
+        </li>
+        <li style="font-size: 1.6rem; color:#333;">
+          {{dept}}
+        </li>
+        <li style="font-size: 1.6rem; color:#333;">
+          {{user_id}}
+        </li>
+      </ul>    
+      <router-link to="/" class="btn black">Back</router-link>
+      <button @click="deleteUser" class="btn red">Delete</button>
    </div>
 </template>
 
 <script>
+  import db from './firebaseInit'
     export default {
         name: 'ViewUser',
         data () {
             return {
-                masthead : 'View User'
+                masthead: 'User',
+                user_id: null,
+                name: null,
+                dept: null,
+                position: null
             }
+        },
+        beforeRouteEnter: (to, from, next) =>{
+          db.collection('users').where('user_id', '==', to.params.user_id).get().then(snap => {
+            snap.forEach(doc => {
+              next(vm => {
+                vm.user_id = doc.data().user_id
+                vm.name = doc.data().name
+                vm.dept = doc.data().dept
+                vm.position = doc.data().position
+              })
+            })
+          })
+        },
+        watch: {
+          '$route': 'fetchData'
+        },
+        methods: {
+          fetchData () {
+            db.collection('users').where('user_id', '==', this.$route.params.user_id).get().then(snap => {
+              snap.forEach(doc => {
+                this.user_id = doc.data().user_id
+                this.position = doc.data().position
+                this.dept = doc.data().dept
+                this.name = doc.data().name
+              })
+            })
+          },
+          deleteUser () {
+            if(confirm('Are you sure?')) {
+              db.collection('users').where('user_id', '==', this.$route.params.user_id).get().then(snap => {
+              snap.forEach(doc => {
+                doc.ref.delete()
+                this.$router.push('/')
+              })
+            })
+            }
+          }
         }
     }
 </script>
